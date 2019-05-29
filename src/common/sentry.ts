d455d8f766db1090e340f7a6eac1684a059a47c8
Amir Moualem <amoualem@gmail.com>
2019-05-29 16:17:47 +0300

    chore: cleaning more redundant files

--- src/common/sentry.ts
+++
@@ -1,9 +0,0 @@
-import Sentry = require('@sentry/node');
-import config = require('./config');
-
-Sentry.init({
-  dsn: config.SENTRY_DSN,
-  maxBreadcrumbs: 20,
-});
-
-export = Sentry;
--- src/common/types.ts
+++
@@ -1,5 +0,0 @@
-import * as express from 'express';
-
-export interface Request extends express.Request {
-  logContext?: any;
-}
