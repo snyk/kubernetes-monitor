28287eb3486e1979f964c409f584e6c6d96a8024
Amir Moualem <amoualem@gmail.com>
2019-05-29 16:10:10 +0300

    chore: removing unnecessary bits
    - config files we don't expect to be used
    - we don't expect to monitor our app with newrelic
    - no need to deploy a service, at least not like our other services, for now
    - vscode-specific files shouldn't be checked in, and should be ignored

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  collectCoverageFrom: [ 'src/**/*.ts' ],
-  coverageReporters: ['text-summary', 'html'],
-  setupTestFrameworkScriptFile: './jest.setup.js'
-};
