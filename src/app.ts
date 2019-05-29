ad9441c1b100269ee1a1d2efd7c5406fa95b4cdf
Amir Moualem <amoualem@gmail.com>
2019-05-29 16:44:53 +0300

    chore: fixing indentations

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
-// import { spawn } from "child_process";
 import KubeApiWrapper = require('./lib/kube-scanner');
 
 export async function monitor(config, logger) {
   setTimeout(() => {
     setInterval(async () => {
-        const scan_output = await KubeApiWrapper.scan();
-        console.log(scan_output);
+      const scan_output = await KubeApiWrapper.scan();
+      console.log(scan_output);
     }, config.MONITOR.SCAN_INTERVAL);
   }, config.MONITOR.INITIAL_REFRESH);
 }
