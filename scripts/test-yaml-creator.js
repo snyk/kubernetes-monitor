b1880667d3ef835c400c187222603ca8cae67634
ivanstanev <istanev93@gmail.com>
2019-06-14 09:55:22 +0200

    test: modify tests to use new yaml names

--- scripts/test-yaml-creator.js
+++ scripts/test-yaml-creator.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const YAML = require('yaml');
 
-const file = fs.readFileSync('./egg-deployment.yaml', 'utf8');
+const file = fs.readFileSync('./snyk-monitor-deployment.yaml', 'utf8');
 const parsedYAML = YAML.parse(file);
 parsedYAML.spec.template.spec.containers[0].image = 'snyk-k8s-monitor:test';
 parsedYAML.spec.template.spec.containers[0].imagePullPolicy = 'Never';
-fs.writeFileSync('egg-test-deployment.yaml', YAML.stringify(parsedYAML));
+fs.writeFileSync('snyk-monitor-test-deployment.yaml', YAML.stringify(parsedYAML));
