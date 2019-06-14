65ac8e9fdca177d8e215e5c712c0c34f6eff1a73
ivanstanev <istanev93@gmail.com>
2019-06-14 15:22:28 +0200

    fix: send only parent workloads
    The monitor was initially written to send every workload that is a direct parent of a pod.
    However, this is not the correct approach, and it results uploading loads of temporary objects (temporary in the sense that they are tied to a larger object, like a deployment, so a re-deployment effectively erases the object).
    
    For example, if we had this structure:
    Deployment -> 2x ReplicaSet -> Pods
    We would be sending the 2x ReplicaSets. Instead, we want the Deployment!
    
    This change attempts to recursively scan up until it finds the topmost workload -- and this is the one that gets sent to homebase.
    The lookup stops whenever there are no more parents, or whenever it finds an unsupported workload.
    
    For example:
    Deployment -> UnsupportedWorkload -> ReplicaSet -> Pod
    If we start from the Pod and look up, we will stop at the UnsupportedWorkload and consider the ReplicaSet the workload that can be inspected.

--- src/lib/kube-scanner/types.ts
+++ src/lib/kube-scanner/types.ts
@@ -1,4 +1,13 @@
-import { Apps_v1Api, Batch_v1Api, Batch_v1beta1Api, Core_v1Api, KubeConfig } from '@kubernetes/client-node';
+import { Apps_v1Api, Batch_v1Api, Batch_v1beta1Api, Core_v1Api, KubeConfig,
+  V1Container, V1ObjectMeta, V1OwnerReference } from '@kubernetes/client-node';
+
+export interface KubeObjectMetadata {
+  kind: string;
+  objectMeta: V1ObjectMeta;
+  specMeta: V1ObjectMeta;
+  containers: V1Container[];
+  ownerRefs: V1OwnerReference[];
+}
 
 export interface IK8sClients {
   readonly appsClient: Apps_v1Api;
@@ -11,7 +20,7 @@ export class K8sClients implements IK8sClients {
   public readonly appsClient: Apps_v1Api;
   public readonly coreClient: Core_v1Api;
   public readonly batchClient: Batch_v1Api;
-  // Keep an eye on this! We need v1beta1 API for CronJobs.
+  // TODO: Keep an eye on this! We need v1beta1 API for CronJobs.
   // https://kubernetes.io/docs/concepts/overview/kubernetes-api/#api-versioning
   // CronJobs will appear in v2 API, but for now there' only v2alpha1, so it's a bad idea to use it.
   public readonly batchUnstableClient: Batch_v1beta1Api;
