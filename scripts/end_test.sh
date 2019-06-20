aeaff4abac11be9724349bf344c7dcccb2cc2c2e
ivanstanev <istanev93@gmail.com>
2019-06-20 17:57:34 +0100

    test: ensure monitor scans workloads in cluster
    Create a few sample deployments alongside the snyk-monitor.
    Ensure the egg scans the deployments and tries to POST the results to Homebase (by inspecting its logs).
    
    Also during test, assign the 8.8.8.8 DNS server to the snyk-monitor pods -- this fixes a weird bug with Travis where name resolution doesn't happen in the container.
    
    Replace the shell scripts with proper test scripts that are now executed as Travis build stages.
    
    Remove config.test.json, use its values directly in the tests.

--- scripts/end_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo 'Test finished. Doing cleanup'
-./kind delete cluster --name=kind
-unset KUBECONFIG
-rm snyk-monitor-test-deployment.yaml
-echo 'Test Done!'
--- test/integration/kubernetes.test.ts
+++ test/integration/kubernetes.test.ts
@@ -1,41 +1,90 @@
-import k8s = require('@kubernetes/client-node');
-import { exec } from 'child-process-promise';
-import path = require('path');
+import { Core_v1Api, KubeConfig } from '@kubernetes/client-node';
+import needle = require('needle');
 import sleep = require('sleep-promise');
-import { test } from 'tap';
+import setup = require('../setup'); // Must be located before 'tap' import
+// tslint:disable-next-line: ordered-imports
+import * as tap from 'tap';
 import * as config from '../../src/common/config';
+import { IWorkloadLocator } from '../../src/transmitter/types';
+import { getKindConfigPath } from '../helpers/kind';
 
-const kc = new k8s.KubeConfig();
+let integrationId: string;
 
-const getKindConfigPath = async () => {
-  try {
-    const parentDir = path.resolve(process.cwd());
-    const kindPath = await exec('./kind get kubeconfig-path', {cwd: parentDir});
-    return kindPath.stdout.replace(/[\n\t\r]/g, '');
-  } catch (err) {
-    throw new Error(`Couldn't execute proccess: ${err}`);
-  }
-};
+tap.tearDown(async () => {
+  console.log('Begin removing the snyk-monitor...');
+  await tap.removeMonitor();
+  console.log('Removed the snyk-monitor!');
+});
 
-test('egg monitor container started', async (t) => {
+// Make sure this runs first -- deploying the monitor for the next tests
+tap.test('deploy snyk-monitor', async (t) => {
+  console.log('Begin deploying the snyk-monitor...');
+
+  t.plan(1);
+
+  integrationId = await tap.deployMonitor();
+
+  console.log(`Deployed the snyk-monitor with integration ID ${integrationId}!`);
+  t.pass('successfully deployed the snyk-monitor');
+});
+
+tap.test('snyk-monitor container started', async (t) => {
+  t.plan(3);
+
+  console.log('Getting KinD config...');
+  const kindConfigPath = await getKindConfigPath();
+  const kubeConfig = new KubeConfig();
+  kubeConfig.loadFromFile(kindConfigPath);
+  const k8sApi = kubeConfig.makeApiClient(Core_v1Api);
+  console.log('Loaded KinD config!');
+
   // wait to let the container go through a cycle
   await sleep(config.MONITOR.INITIAL_REFRESH_MS);
-  const kindPath = await getKindConfigPath();
-  kc.loadFromFile(kindPath);
-  const k8sApi = kc.makeApiClient(k8s.Core_v1Api);
-  t.notEqual(
-    kc.getCurrentCluster(),
-    undefined,
-    'Kind cluster instance is not empty',
-  );
+
+  console.log('Querying the snyk-monitor...');
   const response = await k8sApi.listNamespacedPod('snyk-monitor');
   t.ok(response.body.items.length > 0, 'PodList is not empty');
-  for (const item of response.body.items) {
-    if (item.metadata.namespace.startsWith('kube')) {
-      continue;
+
+  const monitorPod = response.body.items.find((pod) => pod.metadata.name.includes('snyk-monitor'));
+  t.ok(monitorPod !== undefined, 'Snyk monitor container exists');
+  t.notEqual(monitorPod!.status.phase, 'Failed', 'Snyk monitor container didn\'t fail');
+  console.log('Done -- snyk-monitor exists!');
+});
+
+tap.test('snyk-monitor sends data to homebase', async (t) => {
+  t.plan(1);
+
+  console.log(`Begin polling Homebase for the expected workloads with integration ID ${integrationId}...`);
+  // We don't want to spam Homebase with requests; do it infrequently
+  const toneDownFactor = 5;
+  let podStartChecks = setup.KUBERNETES_MONITOR_MAX_WAIT_TIME_SECONDS / toneDownFactor;
+  // TODO: consider if we're OK to expose this publicly?
+  const url = `https://${config.INTERNAL_PROXY_CREDENTIALS}@homebase-int.dev.snyk.io/api/v1/workloads/${integrationId}`;
+  while (podStartChecks-- > 0) {
+    const homebaseResponse = await needle('get', url, null)
+      .catch((error) => t.fail(error));
+
+    const responseBody = homebaseResponse.body;
+    const workloads: IWorkloadLocator[] | undefined = responseBody.workloads;
+
+    if (workloads !== undefined && workloads.length === 3 &&
+        workloads.every((workload) => workload.userLocator === integrationId) &&
+        workloads.every((workload) => workload.cluster === 'inCluster') &&
+        workloads.find((workload) => workload.name === 'alpine' &&
+          workload.type === 'Pod' && workload.namespace === 'services') &&
+        workloads.find((workload) => workload.name === 'nginx' &&
+          workload.type === 'ReplicationController' && workload.namespace === 'services') &&
+        workloads.find((workload) => workload.name === 'redis'
+          && workload.type === 'Deployment' && workload.namespace === 'services')) {
+      break;
     }
-    t.ok(item.metadata.name.includes('snyk-monitor'), 'Snyk monitor container exists');
-    // All pod phases could be found here: https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle/#pod-phase
-    t.notEqual(item.status.phase, 'Failed', `Snyk monitor container didn't fail`);
+
+    await sleep(1000 * toneDownFactor);
+  }
+  if (podStartChecks <= 0) {
+    t.fail('The snyk-monitor did not send data to homebase in the expected timeframe');
+  } else {
+    t.pass('k8s monitor successfully sent expected data to homebase');
+    console.log('Done -- snyk-monitor sent data to Homebase!');
   }
 });
