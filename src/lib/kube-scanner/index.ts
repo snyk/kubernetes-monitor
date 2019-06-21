3a896f3f3d0b8299120608f16df9e608ce3599ca
ivanstanev <istanev93@gmail.com>
2019-06-21 17:37:15 +0100

    feat: watch on Pod changes
    Remove initial scan from the monitor.
    Add a Watch on the Pods of the cluster/namespace. This watch picks up all current Pods once the monitor starts (so it acts the same as a first-scan).
    Ensures images (not tagged as latest) do not get processed more than once.
    
    Also add watch permission to yaml files.
    This allows the monitor to listen to changes to workloads (Pods).
    
    Also pass namespace to the monitor.
    The current namespace within the monitor cannot be inferred, so we have to pass it somehow.
    This change prepares the monitor to receive it via an environment variable.

--- src/lib/kube-scanner/index.ts
+++ src/lib/kube-scanner/index.ts
@@ -5,22 +5,13 @@
  * IMPORTANT:
  * see: https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.14/#-strong-api-overview-strong-
  */
-import { V1PodList } from '@kubernetes/client-node';
 import { sendDepGraph } from '../../transmitter';
 import { IDepGraphPayload, IKubeImage, IScanResponse } from '../../transmitter/types';
 import { pullImages } from '../images';
-import { k8sApi } from './cluster';
 import { constructHomebaseWorkloadPayloads, scanImages, ScanResult } from './image-scanner';
-import { buildMetadataForWorkload } from './metadata-extractor';
 
-// we should not be concerned with k8s-related namespaces
-const IgnoredNamespace = 'kube';
-
-class KubeApiWrapper {
-  public static async scan(): Promise<IScanResponse> {
-    console.log('Begin querying all workloads...');
-    const workloadMetadata = await this.getAllWorkloads();
-
+export = class WorkloadWorker {
+  public static async process(workloadMetadata: IKubeImage[]): Promise<IScanResponse> {
     const allImages = workloadMetadata.map((meta) => meta.imageName);
     console.log(`Queried ${allImages.length} workloads: ${allImages}.`);
     const uniqueImages = [...new Set<string>(allImages)];
@@ -45,44 +36,4 @@ class KubeApiWrapper {
       pulledImages.includes(meta.imageName));
     return { imageMetadata: pulledImageMetadata };
   }
-
-  private static async getAllWorkloads(): Promise<IKubeImage[]> {
-    let imagesInAllNamespaces: IKubeImage[] = [];
-
-    let allPodsResponse: { body: V1PodList };
-    try {
-      allPodsResponse = await k8sApi.coreClient.listPodForAllNamespaces();
-    } catch (error) {
-      console.log(`Could not list pods for all namespaces: ${error.message}`);
-      throw error;
-    }
-
-    if (!allPodsResponse || !allPodsResponse.body) {
-      return imagesInAllNamespaces;
-    }
-
-    for (const pod of allPodsResponse.body.items) {
-      if (pod.metadata.namespace.startsWith(IgnoredNamespace)) {
-        continue;
-      }
-
-      try {
-        const imagesMetadata = await buildMetadataForWorkload(pod);
-        if (imagesMetadata !== undefined && imagesMetadata.length > 0) {
-          imagesInAllNamespaces = imagesInAllNamespaces.concat([...imagesMetadata]);
-        }
-      } catch (error) {
-        const errorMessage = error.response
-          ? `${error.response.statusCode} ${error.response.statusMessage}`
-          : error.message;
-        const images = pod.spec.containers.map((container) => container.image).join(', ');
-        console.log(`Could not build image metadata for pod ${pod.metadata.name}: ${errorMessage}`);
-        console.log(`The pod uses the following images: ${images}`);
-      }
-    }
-
-    return imagesInAllNamespaces;
-  }
-}
-
-export = KubeApiWrapper;
+};
