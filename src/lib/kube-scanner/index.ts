15195f78043026a9665c0ee2debf8004c36bf1c7
ivanstanev <istanev93@gmail.com>
2019-07-02 16:46:44 +0100

    fix: add log ID to logs during image processing
    The snyk-monitor can receive hundreds of requests to process K8s workloads and logs are very hard to follow.
    Prepend a log ID when processing images so that we can connect the sequence of executed steps.

--- src/lib/kube-scanner/index.ts
+++ src/lib/kube-scanner/index.ts
@@ -11,26 +11,32 @@ import { pullImages } from '../images';
 import { constructHomebaseWorkloadPayloads, scanImages, ScanResult } from './image-scanner';
 
 export = class WorkloadWorker {
-  public static async process(workloadMetadata: IKubeImage[]): Promise<IScanResponse> {
+  private readonly logId: string;
+
+  constructor(logId: string) {
+    this.logId = logId;
+  }
+
+  public async process(workloadMetadata: IKubeImage[]): Promise<IScanResponse> {
     const allImages = workloadMetadata.map((meta) => meta.imageName);
-    console.log(`Queried ${allImages.length} workloads: ${allImages}.`);
+    console.log(`${this.logId}: Queried ${allImages.length} workloads: ${allImages}.`);
     const uniqueImages = [...new Set<string>(allImages)];
 
-    console.log('Begin pulling images...');
+    console.log(`${this.logId}: Begin pulling images...`);
     const pulledImages = await pullImages(uniqueImages);
-    console.log(`Pulled ${pulledImages.length} images: ${pulledImages}.`);
+    console.log(`${this.logId}: Pulled ${pulledImages.length} images: ${pulledImages}.`);
 
-    console.log('Begin scanning images...');
+    console.log(`${this.logId}: Begin scanning images...`);
     const scannedImages: ScanResult[] = await scanImages(pulledImages);
-    console.log(`Scanned ${scannedImages.length} images: ${scannedImages.map((image) => image.image)}.`);
+    console.log(`${this.logId}: Scanned ${scannedImages.length} images: ${scannedImages.map((image) => image.image)}.`);
 
-    console.log('Begin constructing payloads...');
+    console.log(`${this.logId}: Begin constructing payloads...`);
     const homebasePayloads: IDepGraphPayload[] = constructHomebaseWorkloadPayloads(scannedImages, workloadMetadata);
-    console.log(`Constructed ${homebasePayloads.length} payloads.`);
+    console.log(`${this.logId}: Constructed ${homebasePayloads.length} payloads.`);
 
-    console.log('Sending dep-graphs to homebase...');
+    console.log(`${this.logId}: Sending dep-graphs to homebase...`);
     await sendDepGraph(...homebasePayloads);
-    console.log('Done!');
+    console.log(`${this.logId}: Done!`);
 
     const pulledImageMetadata = workloadMetadata.filter((meta) =>
       pulledImages.includes(meta.imageName));
