bc3059bd588518daaa3cd661da72bc1749cbebbb
ivanstanev <istanev93@gmail.com>
2019-07-02 16:46:44 +0100

    fix: add log ID to logs during image processing
    The snyk-monitor can receive hundreds of requests to process K8s workloads and logs are very hard to follow.
    Prepend a log ID when processing images so that we can connect the sequence of executed steps.

--- src/lib/kube-scanner/watchers/handlers.ts
+++ src/lib/kube-scanner/watchers/handlers.ts
@@ -1,9 +1,12 @@
 import { V1Pod } from '@kubernetes/client-node';
+import * as uuidv4 from 'uuid/v4';
 import WorkloadWorker = require('../../../lib/kube-scanner');
 import { buildMetadataForWorkload } from '../metadata-extractor';
 import { WatchEventType } from './types';
 
 export async function podWatchHandler(eventType: string, pod: V1Pod) {
+  const logId = uuidv4().substring(0, 8);
+
   if (eventType === WatchEventType.Deleted) {
     return;
   }
@@ -13,22 +16,23 @@ export async function podWatchHandler(eventType: string, pod: V1Pod) {
 
     if (workloadMetadata === undefined || workloadMetadata.length === 0) {
       const imageNames = pod.spec.containers.map((container) => container.image);
-      console.log(`Could not process the images for Pod ${pod.metadata.name}! The workload is possibly unsupported.` +
-        `The pod's spec has the following images: ${imageNames}`);
+      console.log(`${logId}: Could not process the images for Pod ${pod.metadata.name}!` +
+        `The workload is possibly unsupported. The pod's spec has the following images: ${imageNames}`);
       return;
     }
 
-    const processedImages = await WorkloadWorker.process(workloadMetadata);
+    const workloadWorker = new WorkloadWorker(logId);
+    const processedImages = await workloadWorker.process(workloadMetadata);
 
     const processedImageNames = processedImages.imageMetadata.map((image) => image.imageName);
-    console.log(`Processed the following images: ${processedImageNames}.`);
+    console.log(`${logId}: Processed the following images: ${processedImageNames}.`);
   } catch (error) {
     const errorMessage = error.response
       ? `${error.response.statusCode} ${error.response.statusMessage}`
       : error.message;
     const imageNames = pod.spec.containers.map((container) => container.image);
 
-    console.log(`Could not build image metadata for pod ${pod.metadata.name}: ${errorMessage}`);
-    console.log(`The pod uses the following images: ${imageNames}`);
+    console.log(`${logId}: Could not build image metadata for pod ${pod.metadata.name}: ${errorMessage}`);
+    console.log(`${logId}: The pod uses the following images: ${imageNames}`);
   }
 }
