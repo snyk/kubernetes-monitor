6aee55abc8bac65b3ed37b94fbd7dc2ff1894198
Amir Moualem <amoualem@gmail.com>
2019-07-10 11:01:27 +0100

    test: unit tests for isPodAssociatedWithParent
    just asserting existing behaviour before removing usage of lodash.

---
+++ test/unit/metadata-extractor.test.ts
@@ -0,0 +1,56 @@
+import * as tap from 'tap';
+
+import { V1OwnerReference, V1Pod } from '@kubernetes/client-node';
+
+import metadataExtractor = require('../../src/lib/kube-scanner/metadata-extractor');
+
+tap.test('isPodAssociatedWithParent', async (t) => {
+  const mockPodWithoutMetadata = {};
+  t.throws(() => metadataExtractor.isPodAssociatedWithParent(mockPodWithoutMetadata as V1Pod),
+    'isPodAssociatedWithParent throws an error on pod without metadata');
+
+  const mockPodWithEmptyMetadata = {metadata: {}};
+  const isPodWithEmptyMetadataAssociatedWithParent = metadataExtractor.isPodAssociatedWithParent(
+    mockPodWithEmptyMetadata as V1Pod);
+  t.notOk(isPodWithEmptyMetadataAssociatedWithParent, 'pod with empty metadata is not associated with parent');
+
+  const mockPodWithEmptyOwnerReferences = {metadata: {ownerReferences: [] as V1OwnerReference[]}};
+  const isPodWithEmptyOwnerReferenceAssociatedWithParent = metadataExtractor.isPodAssociatedWithParent(
+    mockPodWithEmptyOwnerReferences as V1Pod);
+  t.notOk(isPodWithEmptyOwnerReferenceAssociatedWithParent,
+    'pod with empty owner references is not associated with parent');
+
+  const mockPodWithOwnerReferencesWithoutKind = {metadata: {ownerReferences: [
+    {} as V1OwnerReference,
+  ] as V1OwnerReference[]}};
+  const isPodWithOwnerReferencesWithoutKindAssociatedWithParent = metadataExtractor.isPodAssociatedWithParent(
+    mockPodWithOwnerReferencesWithoutKind as V1Pod);
+  t.notOk(isPodWithOwnerReferencesWithoutKindAssociatedWithParent,
+    'pod with owner references without kind is not associated with parent');
+
+  const mockPodWithOwnerReferencesWithEmptyKInd = {metadata: {ownerReferences: [
+    {kind: ''} as V1OwnerReference,
+  ] as V1OwnerReference[]}};
+  const isPodWithOwnerReferencesWithEmptyKindAssociatedWithParent = metadataExtractor.isPodAssociatedWithParent(
+    mockPodWithOwnerReferencesWithEmptyKInd as V1Pod);
+  t.notOk(isPodWithOwnerReferencesWithEmptyKindAssociatedWithParent,
+    'pod with owner references with empty kind is not associated with parent');
+
+  const mockPodWithOwnerReferencesWithKind = {metadata: {ownerReferences: [
+    {kind: 'BUTTER'} as V1OwnerReference,
+  ] as V1OwnerReference[]}};
+  const isPodWithOwnerReferencesWithKindAssociatedWithParent = metadataExtractor.isPodAssociatedWithParent(
+    mockPodWithOwnerReferencesWithKind as V1Pod);
+  t.ok(isPodWithOwnerReferencesWithKindAssociatedWithParent,
+    'pod with owner references with kind is associated with parent');
+
+  const mockPodWithMixedOwnerReferences = {metadata: {ownerReferences: [
+    {} as V1OwnerReference,
+    {kind: ''} as V1OwnerReference,
+    {kind: 'BUTTER'} as V1OwnerReference,
+  ] as V1OwnerReference[]}};
+  const isPodWithMixedOwnerReferencesAssociatedWithParent = metadataExtractor.isPodAssociatedWithParent(
+    mockPodWithMixedOwnerReferences as V1Pod);
+  t.ok(isPodWithMixedOwnerReferencesAssociatedWithParent,
+    'pod with some owner references with kind is associated with parent');
+});
