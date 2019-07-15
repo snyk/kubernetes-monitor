3a4785e588805ee65790bd7880fc3bcc67945978
Tal Kaptsan <kaptsan.tal@hotmail.com>
2019-07-15 15:54:46 +0300

    fix: scan pod only if its running
    In order to lower the amount of scans, we want to scan only on the last event when the Pod is finally ready and K8s won’t fire more events after that

--- src/lib/kube-scanner/watchers/types.ts
+++ src/lib/kube-scanner/watchers/types.ts
@@ -1,4 +1,20 @@
 export enum WatchEventType {
   Added = 'ADDED',
+  Modified = 'MODIFIED',
   Deleted = 'DELETED',
+  Bookmark = 'BOOKMARK',
+  Error = 'ERROR',
+}
+
+export enum PodPhase {
+  // The pod has been accepted by the Kubernetes system, but one or more of the container images has not been created.
+  Pending = 'Pending',
+  // The pod has been bound to a node, and all of the containers have been created.
+  Running = 'Running',
+  // All containers in the pod have terminated in success, and will not be restarted.
+  Succeeded = 'Succeeded',
+  // All containers in the pod have terminated, and at least one container has terminated in failure.
+  Failed = 'Failed',
+  // For some reason the state of the pod could not be obtained.
+  Unknown = 'Unknown',
 }
