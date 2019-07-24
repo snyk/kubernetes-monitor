562b8409145a9a24732f830a14a6336417b793fb
ivanstanev <istanev93@gmail.com>
2019-07-24 09:44:15 +0100

    fix: log errors on failed API calls

--- src/transmitter/index.ts
+++ src/transmitter/index.ts
@@ -2,17 +2,26 @@ import needle = require('needle');
 import * as config from '../common/config';
 import { IDepGraphPayload } from './types';
 
+function isSuccessStatusCode(statusCode: number) {
+  return statusCode > 100 && statusCode < 400;
+}
+
 export async function sendDepGraph(...payloads: IDepGraphPayload[]) {
   for (const payload of payloads) {
     try {
-      await needle('post', `${config.HOMEBASE.url}/api/v1/dependency-graph`, payload, {
+      const result = await needle('post', `${config.HOMEBASE.url}/api/v1/dependency-graph`, payload, {
           json: true,
           compressed: true,
         },
       );
+
+      if (!isSuccessStatusCode(result.statusCode!)) {
+        const httpErrorText = `Server error: ${result.statusCode} ${result.statusMessage}.`;
+        const payloadText = `Payload: ${JSON.stringify(payload)}`;
+        throw new Error(`${httpErrorText} ${payloadText}`);      }
     } catch (error) {
       const errorMessage = error.message ? error.message : error;
-      console.log(`Could not send data to Homebase: ${errorMessage}`);
+      console.log(`Could not send the dependency scan result to Homebase: ${errorMessage}`);
     }
   }
 }
