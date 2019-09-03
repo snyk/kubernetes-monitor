37c1124723ae33ccc3ed064de5bcc0b6596e4080
ivanstanev <istanev93@gmail.com>
2019-09-03 11:45:36 +0100

    fix: log workload name instead of random log ID
    Initially the idea of logging a random ID was to distinguish different workload processing flows as many of them ran concurrently and created log messages.
    Sometimes it was difficult to follow for example where 3 of the same "Scanning pulled images" messages come from.
    
    Logging a random ID actually doesn't give us any meaningful information; instead we can log the workload name, which is already unique as we opt to scan an image only once.
    It should be much clearer to also figure out which workloads have been processed by the monitor and we can verify that they must appear in Snyk import UI.

--- src/kube-scanner/watchers/handlers/deployment.ts
+++ src/kube-scanner/watchers/handlers/deployment.ts
@@ -1,15 +1,13 @@
 import { V1Deployment } from '@kubernetes/client-node';
-import * as uuidv4 from 'uuid/v4';
 import { WatchEventType } from '../types';
 import { deleteWorkload } from './index';
 import { WorkloadKind } from '../../types';
+import { FALSY_WORKLOAD_NAME_MARKER } from './types';
 
 export async function deploymentWatchHandler(eventType: string, deployment: V1Deployment) {
   if (eventType !== WatchEventType.Deleted) {
     return;
   }
-
-  const logId = uuidv4().substring(0, 8);
 
   if (!deployment.metadata || !deployment.spec || !deployment.spec.template.metadata ||
       !deployment.spec.template.spec) {
@@ -17,11 +15,13 @@ export async function deploymentWatchHandler(eventType: string, deployment: V1De
     return;
   }
 
+  const workloadName = deployment.metadata.name || FALSY_WORKLOAD_NAME_MARKER;
+
   await deleteWorkload({
     kind: WorkloadKind.Deployment,
     objectMeta: deployment.metadata,
     specMeta: deployment.spec.template.metadata,
     containers: deployment.spec.template.spec.containers,
     ownerRefs: deployment.metadata.ownerReferences,
-  }, logId);
+  }, workloadName);
 }
