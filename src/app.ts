6cf5404607e382c34144877704fa523c1828fab8
ivanstanev <istanev93@gmail.com>
2019-10-30 14:17:05 +0000

    fix: don't wait for a while before beginning to scan
    The timeout that we apply before setting up the snyk-monitor is not needed currently, hence removing it so that we don't keep wondering every now and then why it's there.

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,12 @@
-import * as config from './common/config';
 import logger = require('./common/logger');
 import { currentClusterName } from './kube-scanner/cluster';
 import { beginWatchingWorkloads } from './kube-scanner/watchers/namespaces';
 
-function safeMonitoring() {
+export function monitor() {
   try {
     logger.info({cluster: currentClusterName}, 'starting to monitor');
     beginWatchingWorkloads();
   } catch (error) {
     logger.error({error}, 'an error occurred while monitoring the cluster');
   }
-}
-
-export function monitor() {
-  setTimeout(safeMonitoring, config.MONITOR.INITIAL_REFRESH_MS);
 }
