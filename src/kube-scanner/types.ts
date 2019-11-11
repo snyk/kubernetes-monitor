25a34a44bc75cbc0306e3c93dcb4ab34345a196e
Tal Kaptsan <kaptsan.tal@hotmail.com>
2019-11-11 18:11:57 +0200

    feat: add version api to k8s client

--- src/kube-scanner/types.ts
+++ src/kube-scanner/types.ts
@@ -1,5 +1,16 @@
-import { AppsV1Api, BatchV1Api, BatchV1beta1Api, CoreV1Api, KubeConfig,
-  V1ObjectMeta, V1OwnerReference, V1PodSpec } from '@kubernetes/client-node';
+import logger = require('../common/logger');
+import {
+  AppsV1Api,
+  BatchV1Api,
+  BatchV1beta1Api,
+  CoreV1Api,
+  KubeConfig,
+  V1ObjectMeta,
+  V1OwnerReference,
+  V1PodSpec,
+  VersionApi,
+  VersionInfo
+} from '@kubernetes/client-node';
 
 export enum WorkloadKind {
   Deployment = 'Deployment',
@@ -46,6 +57,8 @@ export interface IK8sClients {
   readonly coreClient: CoreV1Api;
   readonly batchClient: BatchV1Api;
   readonly batchUnstableClient: BatchV1beta1Api;
+  readonly versionClient: VersionApi;
+  getK8sClientVersion(): Promise<string | undefined>;
 }
 
 export class K8sClients implements IK8sClients {
@@ -56,11 +69,24 @@ export class K8sClients implements IK8sClients {
   // https://kubernetes.io/docs/concepts/overview/kubernetes-api/#api-versioning
   // CronJobs will appear in v2 API, but for now there' only v2alpha1, so it's a bad idea to use it.
   public readonly batchUnstableClient: BatchV1beta1Api;
+  public readonly versionClient: VersionApi;
 
   constructor(config: KubeConfig) {
     this.appsClient = config.makeApiClient(AppsV1Api);
     this.coreClient = config.makeApiClient(CoreV1Api);
     this.batchClient = config.makeApiClient(BatchV1Api);
     this.batchUnstableClient = config.makeApiClient(BatchV1beta1Api);
+    this.versionClient = config.makeApiClient(VersionApi);
+  }
+
+  public async getK8sClientVersion(): Promise<string | undefined> {
+    try {
+      const result = await this.versionClient.getCode();
+      const versionInfo: VersionInfo = result.body;
+      return versionInfo.gitVersion;
+    } catch (error) {
+      logger.warn({error}, 'failed to get kubernetes api version');
+      return;
+    }
   }
 }
