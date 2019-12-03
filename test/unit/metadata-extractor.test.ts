46050fdd7259b319ae8437f6085f579c6ecde5cf
Amir Moualem <amoualem@gmail.com>
2019-12-03 14:40:49 +0200

    test: discrepancies between owner spec and child statuses
    metadataExtractor.buildImageMetadata accepts a workload's metadata and a list of container statuses, belonging to containers in a pod running said images.
    this function attempts to build image metadata based on the owner's spec (for example a deployment), which contains the data about "what should be", as well as the container statuses for that pod, which contain data about "what actually is".
    
    some discrepancies may occur between "what should be" and "what actually is".
    an example we've stumbled upon happens when sidecar containers are injected (through an admission controller), causing the deployment's spec to contain a single container, but the statuses to include the injected containers.
    
    this results in an error in buildImageMetadata that relies every container that appears in the statuses list to also appear in the spec.
    
    this test proves the bug exists.

--- test/unit/metadata-extractor.test.ts
+++ test/unit/metadata-extractor.test.ts
@@ -1,6 +1,9 @@
 import * as tap from 'tap';
+import * as fs from 'fs';
+import * as YAML from 'yaml';
 
-import { V1OwnerReference, V1Pod } from '@kubernetes/client-node';
+import { V1OwnerReference, V1Pod, V1Deployment } from '@kubernetes/client-node';
+import * as scannerTypes from '../../src/kube-scanner/types';
 
 import * as metadataExtractor from '../../src/kube-scanner/metadata-extractor';
 
@@ -53,4 +56,24 @@ tap.test('isPodAssociatedWithParent', async (t) => {
     mockPodWithMixedOwnerReferences as V1Pod);
   t.ok(isPodWithMixedOwnerReferencesAssociatedWithParent,
     'pod with some owner references with kind is associated with parent');
+});
+
+tap.test('buildImageMetadata', async (t) => {
+  const deploymentFixture = fs.readFileSync('./test/fixtures/sidecar-containers/deployment.yaml', 'utf8');
+  const deploymentObject: V1Deployment = YAML.parse(deploymentFixture);
+  const podFixture = fs.readFileSync('./test/fixtures/sidecar-containers/pod.yaml', 'utf8');
+  const podObject: V1Pod = YAML.parse(podFixture);
+
+  const deploymentWeirdWrapper: scannerTypes.IKubeObjectMetadata = {
+    kind: 'Deployment',
+    objectMeta: deploymentObject.metadata!,
+    specMeta: deploymentObject.spec!.template.metadata!,
+    ownerRefs: deploymentObject.metadata!.ownerReferences,
+    podSpec: deploymentObject.spec!.template.spec!,
+  }
+
+  t.throws(() => metadataExtractor.buildImageMetadata(
+    deploymentWeirdWrapper,
+    podObject.status!.containerStatuses!,
+  ), 'buildImageMetadata can\'t handle discrepancies between spec and status');
 });
