9becd7bf56e345cd1b33802bc2d4ed44c9568f69
Amir Moualem <amoualem@gmail.com>
2019-12-10 15:38:48 +0200

    chore: cleanup unused ci&cd notifications
    when we used Travis, we had a workflow/job/command/egg to handle cases where PRs are open from staging to the master branch.
    this flow has been irrelevant since our migration to CircleCI.
    removing its unused references and the script it used to invoke.

--- scripts/slack-notify-pr.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-curl -X POST -H 'Content-Type:application/json' -d '{"attachments": [{"color": "warning", "fallback": "Build Notification: '$CIRCLE_BUILD_URL'", "title": "Kubernetes-Monitor Publish Notification", "text": ":egg: A new version is about to be published! :egg:\n'$CIRCLE_PULL_REQUEST'\nbranch of origin is `TODO`"}]}' $SLACK_WEBHOOK
