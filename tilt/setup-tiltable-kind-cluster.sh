1dc46291c9180dd2afe78f11a864ae2fa3823db6
Amir Moualem <amoualem@gmail.com>
2020-01-12 11:18:10 +0200

    chore: outdated tilt/KinD setup, better to just remove it

--- tilt/setup-tiltable-kind-cluster.sh
+++
@@ -1,8 +0,0 @@
-./kind create cluster --name=kind
-export KUBECONFIG=`./kind get kubeconfig-path`
-./kubectl create namespace snyk-monitor
-./kubectl create secret generic snyk-monitor -n snyk-monitor --from-literal=dockercfg.json="{}" --from-literal=integrationId="41214617-cb5d-4674-8d97-5b456952c360"
-echo '------------------------------------------------'
-echo 'run "PATH=$PATH:./ tilt up" to start tilt'
-echo "don't forget to set up KUBECONFIG"
-echo 'clean up afterwards with "./kind delete cluster"'
