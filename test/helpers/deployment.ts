8112652c165ddb2468e1ec50d4b246c45805f5ba
ivanstanev <istanev93@gmail.com>
2020-01-29 09:51:05 +0000

    chore: relax checks on runAsUser and runAsGroup
    To allow our tests to check some security properties at runtime, we need to relax specifically runAsUser and runAsGroup.
    For OpenShift the UID/GID is a very high number but it is also randomised, so there is no need for us to try and predict it and be exact in our tests.
    The change will allow any sufficiently high UID/GID (>=10001), which should still be a good measure of following security best practices.

--- test/helpers/deployment.ts
+++ test/helpers/deployment.ts
@@ -42,8 +42,16 @@ export function validateSecureConfiguration(test: tap, deployment: V1Deployment)
   tap.ok(securityContext.allowPrivilegeEscalation === false, 'must explicitly set allowPrivilegeEscalation to false');
   tap.ok(securityContext.privileged === false, 'must explicitly set privileged to false');
   tap.ok(securityContext.runAsNonRoot === true, 'must explicitly set runAsNonRoot to true');
-  tap.ok(securityContext.runAsUser === 10001, 'must explicitly set runAsUser to 10001');
-  tap.ok(securityContext.runAsGroup === 10001, 'must explicitly set runAsGroup to 10001');
+  tap.ok(
+    securityContext.runAsUser !== undefined &&
+      securityContext.runAsUser >= 10001,
+    'must explicitly set runAsUser to be 10001 or greater',
+  );
+  tap.ok(
+    securityContext.runAsGroup !== undefined &&
+      securityContext.runAsGroup >= 10001,
+    'must explicitly set runAsGroup to be 10001 or greater',
+  );
 }
 
 export function validateVolumeMounts(test: tap, deployment: V1Deployment) {
