5f5b7e97e9adf5d4d29dd7993decbed23df3888b
Arthur Granado <arthur.granado@snyk.io>
2020-02-21 15:43:32 +0000

    chore: check operator files

--- scripts/create-cluster-service-version.sh
+++ scripts/create-cluster-service-version.sh
@@ -23,3 +23,16 @@ SOURCE_CSV="${CSV_LOCATION}/${VERSION_TO_OVERRIDE_IN_CSV}/snyk-operator.v0.0.0.c
 TARGET_CSV="${CSV_LOCATION}/${VERSION_TO_OVERRIDE_IN_CSV}/snyk-operator.v${VERSION_TO_OVERRIDE_IN_CSV}.clusterserviceversion.yaml"
 mv "${SOURCE_CSV}" "${TARGET_CSV}"
 sed -i.bak "s|0.0.0|${VERSION_TO_OVERRIDE_IN_CSV}|g" "${TARGET_CSV}"
+
+# Check if the Operator files are valid
+echo "ls ${CSV_LCATION}"
+ls ${CSV_LOCATION}
+echo "ls ${CSV_LCATION}/0.0.0"
+ls "${CSV_LOCATION}/0.0.0"
+echo "ls ${CSV_LCATION}/${VERSION_TO_OVERRIDE_IN_CSV}"
+ls "${CSV_LOCATION}/${VERSION_TO_OVERRIDE_IN_CSV}"
+pyenv global 3.5.2
+pip3 install --upgrade pip
+pip3 install operator-courier
+cat "${CSV_LOCATION}/snyk-operator.package.yaml"
+operator-courier verify --ui_validate_io ${CSV_LOCATION}
