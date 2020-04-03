15020678f37d54ad04b63ec6815f9f7d92bd7621
agatakrajewska <agata.krajewska@snyk.io>
2020-04-03 16:32:00 +0100

    chore: slack integration scripts rewritten to python

--- scripts/slack/notify-failure-on-branch.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-DISPLAY_BRANCH_NAME="$1"
-
-if [[ "$CIRCLE_BRANCH" == "staging" ]]; then
-  ./scripts/slack/notify-failure.sh "$DISPLAY_BRANCH_NAME"
-else
-  echo "Current branch is $CIRCLE_BRANCH so skipping notifying Slack"
-fi
