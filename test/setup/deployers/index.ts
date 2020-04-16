6b9a2abead9a874bec608ed777128a919d19d21e
Ivan Stanev <istanev93@gmail.com>
2020-04-16 18:14:35 +0100

    test: introduce a test script for an environment with a proxy
    Adds coverage for the Snyk controller operating with a forwarding proxy for HTTPS requests.

--- test/setup/deployers/index.ts
+++ test/setup/deployers/index.ts
@@ -1,9 +1,11 @@
 import { yamlDeployer } from './yaml';
 import { operatorDeployer } from './operator';
 import { helmDeployer } from './helm';
+import { helmWithProxyDeployer } from './helm-with-proxy';
 
 export default {
   YAML: yamlDeployer,
   Operator: operatorDeployer,
   Helm: helmDeployer,
+  Proxy: helmWithProxyDeployer,
 };
