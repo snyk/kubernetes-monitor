ff2a1dea53bccf93868348dc5976e63ec8f6cb12
Arthur Granado <arthur.granado@snyk.io>
2020-07-24 15:12:35 +0100

    Remove it: just for test

--- scripts/operator/delete-operator-from-quay.sh
+++ scripts/operator/delete-operator-from-quay.sh
@@ -3,7 +3,7 @@
 set -e
 
 QUAY_TOKEN=$(curl -H "Content-Type: application/json" -XPOST https://quay.io/cnr/api/v1/users/login -d "{\"user\": {\"username\": \"${QUAY_USERNAME}\", \"password\": \"${QUAY_PASSWORD}\"}}" | jq -r .token)
-OPERATOR_VERSION="0.0.1-${CIRCLE_SHA1}"
+OPERATOR_VERSION="0.0.1-2a4e062254ca15ea4f476ccfde33a5219881d7e6"
 
 STATUSCODE=$( curl --silent --output /dev/stderr --write-out "%{http_code}" -XDELETE -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: $QUAY_TOKEN" "https://quay.io/cnr/api/v1/packages/snyk-runtime/snyk-operator/${OPERATOR_VERSION}/helm" )
 
