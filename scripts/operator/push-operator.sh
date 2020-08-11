71276293d433a3dbec0bba55999bc5ba9a1cdce4
Arthur Granado <arthur.granado@snyk.io>
2020-08-11 14:39:51 +0100

    chore: push snyk operator to operatorhub.io

---
+++ scripts/operator/push-operator.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+#
+# Inputs:
+# - $1: The current path (e.g. "./kubernetes-monitor" or $(pwd))
+# - $2: The community folder that the operator will be pushed (e.g. 'community-operators' or 'upstream-community-operators')
+#
+# Outputs:
+# - Push a new branch to snyk/community-operators
+#
+# Push a new version of the Operator to the Snyk community-operators fork to publish it later.
+# This branch will be ready to open a pull request to operator-framework community-operators repo.
+#
+
+set -xeo pipefail
+
+CURRENT_DIRECTORY=$1
+COMMUNITY_FOLDER_LOCATION=$2
+COMMUNITY_OPERATORS_UPSTREAM_LOCATION="${CURRENT_DIRECTORY}/community-operators"
+DEPLOY_LOCATION="${COMMUNITY_OPERATORS_UPSTREAM_LOCATION}/${COMMUNITY_FOLDER_LOCATION}"
+OPERATOR_LOCATION="${CURRENT_DIRECTORY}/snyk-operator/deploy/olm-catalog/snyk-operator"
+
+# Configure git user and gpg key
+echo "${OPENSHIFT_OPERATOR_SIGNING_KEY_BASE64}" | base64 -d | gpg --import
+git config --global commit.gpgsign true
+git config --global user.signingkey "${OPENSHIFT_OPERATOR_SIGNING_KEY_ID}"
+git config --global user.email "${OPENSHIFT_OPERATOR_GITHUB_EMAIL}"
+git config --global user.name "${OPENSHIFT_OPERATOR_GITHUB_NAME}"
+
+# Clone Community Operators repo from Snyk
+git clone https://github.com/snyk/community-operators.git $COMMUNITY_OPERATORS_UPSTREAM_LOCATION
+cd "${COMMUNITY_OPERATORS_UPSTREAM_LOCATION}"
+git checkout -b snyk/${COMMUNITY_FOLDER_LOCATION}/snyk-operator-v${NEW_OPERATOR_VERSION}
+
+# Create location if it doesn't exist
+mkdir -p  "${DEPLOY_LOCATION}/snyk-operator"
+
+# Copy new release to branch
+cp -r "${OPERATOR_LOCATION}/${NEW_OPERATOR_VERSION}" "${DEPLOY_LOCATION}/snyk-operator/."
+cp "${OPERATOR_LOCATION}/snyk-operator.package.yaml" "${DEPLOY_LOCATION}/snyk-operator/."
+
+# Create the signed commit and push
+git add "${DEPLOY_LOCATION}/snyk-operator/*"
+git commit -s -m "Upgrade snyk-operator to version ${NEW_OPERATOR_VERSION} on ${COMMUNITY_FOLDER_LOCATION}"
+git push --set-upstream origin --force snyk/${COMMUNITY_FOLDER_LOCATION}/snyk-operator-v${NEW_OPERATOR_VERSION}
--- scripts/slack/notify_success_operator_push.py
+++ scripts/slack/notify_success_operator_push.py
@@ -5,19 +5,19 @@ import os
 import requests
 import json
 
-def notifySlack(operator_version):
+def notifySlack(operator_version, upstream_community):
     circle_build_url = os.getenv('CIRCLE_BUILD_URL')
     url = os.getenv('SLACK_WEBHOOK')
 
     data = {
-      'attachments': 
+      'attachments':
       [
         {
           'color': '#7CD197',
           'fallback': 'Build Notification: ' + circle_build_url,
           'title': 'Snyk Operator Pushed to GitHub repo snyk/community-operators',
-          'text': 'Branch *snyk/snyk-operator-v' + operator_version + '* is ready for publishing to the upstream community-operators.\n' +
-            'https://github.com/operator-framework/community-operators/compare/master...snyk:snyk/snyk-operator-v' + operator_version
+          'text': 'Branch *snyk/snyk-operator-v' + operator_version + '* is ready for publishing to the ' + upstream_community + '.\n' +
+            'https://github.com/operator-framework/community-operators/compare/master...snyk:snyk/snyk-operator-v' + operator_version + '-' + upstream_community
         }
       ]
     }
@@ -26,4 +26,5 @@ def notifySlack(operator_version):
 
 if __name__ == '__main__':
     operator_version = sys.argv[1]
-    notifySlack(operator_version)
+    upstream_community = sys.argv[2]
+    notifySlack(operator_version, upstream_community)
