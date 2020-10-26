1c56950b81909b9007617ba09302dc42c39be907
Daniel Kontorovskyi <dan.kontorovskyy@gmail.com>
2020-10-26 17:54:01 +0200

    feat: revert to pulling by tag
    This reverts commit 31c7e4e2f6c589fb7672fc8d3aad56b148b82572, reversing
    changes made to 319e748e423e4c04665097862fc3e4a539fc3959.

--- src/transmitter/payload.ts
+++ src/transmitter/payload.ts
@@ -27,7 +27,6 @@ export function constructDepGraph(
     const imageLocator: IImageLocator = {
       userLocator: config.INTEGRATION_ID,
       imageId: scannedImage.image,
-      imageWithDigest: scannedImage.imageWithDigest,
       cluster,
       namespace,
       type,
