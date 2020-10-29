c8eb4fe41f8bb2c4113f4f21d30840658e25b47c
Ivan Stanev <istanev93@gmail.com>
2020-10-29 13:50:27 +0000

    chore: add more details to Slack message on CircleCI Job failure

--- scripts/slack/notify_failure.py
+++ scripts/slack/notify_failure.py
@@ -5,24 +5,29 @@ import requests
 import json
 import sys
 
-def notifySlack(branch_name):
-    circle_build_url = os.getenv('CIRCLE_BUILD_URL')
-    url = os.getenv('SLACK_WEBHOOK')
+
+def notifySlack(branch_name: str, job_name: str, build_url: str, pr_url: str, slack_webhook: str):
+    job_name_message = 'Job name: `' + job_name + '`\n'
+    build_url_message = 'Build URL: ' + build_url + '\n'
+    pr_url_message = 'Pull request URL: ' + pr_url + '\n'
+    message = ':egg_broken_1: Kubernetes-Monitor broken branch: `' + branch_name + \
+        '` :egg_broken_1:\n' + job_name_message + build_url_message + pr_url_message
 
     data = {
-      'attachments': 
-      [
-        {
-          'color': '#EE0000',
-          'fallback': 'Build Notification: ' + circle_build_url,
-          'title': ':warning: Kubernetes-Monitor Merge Failure :warning:',
-          'text': ':egg_broken_1: Kubernetes-Monitor broken branch: `' + branch_name + '` :egg_broken_1:\n' + circle_build_url
-        }
-      ]
+        'attachments':
+        [
+            {
+                'color': '#EE0000',
+                'fallback': 'Build Notification: ' + build_url,
+                'title': ':warning: Kubernetes-Monitor Merge Failure :warning:',
+                'text': message
+            }
+        ]
     }
 
-    requests.post(url, data=json.dumps(data))
+    requests.post(slack_webhook, data=json.dumps(data))
+
 
 if __name__ == '__main__':
-    branch_name = sys.argv[1]
-    notifySlack(branch_name)
+    _, branch_name, job_name, build_url, pr_url, slack_webhook = sys.argv
+    notifySlack(branch_name, job_name, build_url, pr_url, slack_webhook)
--- scripts/slack/notify_failure_on_branch.py
+++ scripts/slack/notify_failure_on_branch.py
@@ -4,14 +4,16 @@ import os
 from notify_failure import notifySlack
 import sys
 
-def notifyOnBranch(display_branch_name):
-    circle_branch_name = os.getenv('CIRCLE_BRANCH')
 
-    if circle_branch_name == 'staging':
-        notifySlack(display_branch_name)
+def notifyOnBranch(branch_name: str, job_name: str, build_url: str, pr_url: str, slack_webhook: str):
+    if branch_name == 'staging':
+        notifySlack(branch_name, job_name, build_url, pr_url, slack_webhook)
     else:
-        print('Current branch is ' + circle_branch_name + ' so skipping notifying Slack')
+        print('Current branch is ' + branch_name +
+              ' so skipping notifying Slack')
+
 
 if __name__ == '__main__':
+    _, branch_name, job_name, build_url, pr_url, slack_webhook = sys.argv
     display_branch_name = sys.argv[1]
-    notifyOnBranch(display_branch_name)
+    notifyOnBranch(branch_name, job_name, build_url, pr_url, slack_webhook)
