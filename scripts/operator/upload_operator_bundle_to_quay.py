6c165eaf52d3a3ca48b8d14f3b2b3e2181cb7201
Ivan Stanev <istanev93@gmail.com>
2020-10-30 15:44:05 +0000

    chore: automate testing OpenShift

---
+++ scripts/operator/upload_operator_bundle_to_quay.py
@@ -0,0 +1,13 @@
+from subprocess import call
+from sys import argv
+
+
+def uploadOperatorBundleToQuay(operator_dir: str, quay_namespace: str, package_name: str, package_version: str, quay_token: str) -> None:
+    call(["operator-courier", "push", operator_dir, quay_namespace,
+          package_name, package_version, quay_token])
+
+
+if __name__ == '__main__':
+    _, operator_dir, quay_namespace, package_name, package_version, quay_token = argv
+    uploadOperatorBundleToQuay(
+        operator_dir, quay_namespace, package_name, package_version, quay_token)
