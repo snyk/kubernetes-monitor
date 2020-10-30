2de44a000c5370bb2da8820d8b0bf35e9d4efa3b
Ivan Stanev <istanev93@gmail.com>
2020-10-30 15:39:39 +0000

    chore: OperatorSource in tests is now dynamically injected
    Allows to switch to different Quay repositories so that local runs and CI runs do not conflict.

--- test/setup/deployers/operator-openshift.ts
+++ test/setup/deployers/operator-openshift.ts
@@ -1,3 +1,5 @@
+import { readFileSync, writeFileSync } from 'fs';
+import { parse, stringify } from 'yaml';
 import { IDeployer, IImageOptions } from './types';
 import * as kubectl from '../../helpers/kubectl';
 
@@ -8,10 +10,30 @@ export const operatorDeployer: IDeployer = {
 async function deployKubernetesMonitor(
   _imageOptions: IImageOptions,
 ): Promise<void> {
-  await kubectl.applyK8sYaml('./test/fixtures/operator/operator-source.yaml');
+  const overriddenOperatorSource = 'snyk-monitor-operator-source.yaml';
+  createTestOperatorSource(
+    overriddenOperatorSource,
+    process.env['QUAY_USERNAME']!,
+  );
+
+  await kubectl.applyK8sYaml(overriddenOperatorSource);
   await kubectl.applyK8sYaml('./test/fixtures/operator/installation.yaml');
   // Await for the Operator to become available, only then
   // the Operator can start processing the custom resource.
   await kubectl.waitForDeployment('snyk-operator', 'snyk-monitor');
   await kubectl.applyK8sYaml('./test/fixtures/operator/custom-resource.yaml');
+}
+
+function createTestOperatorSource(newYamlPath: string, username: string): void {
+  console.log('Creating YAML OperatorSource...');
+  const originalDeploymentYaml = readFileSync(
+    './test/fixtures/operator/operator-source.yaml',
+    'utf8',
+  );
+  const deployment = parse(originalDeploymentYaml);
+
+  deployment.spec.registryNamespace = username;
+
+  writeFileSync(newYamlPath, stringify(deployment));
+  console.log('Created YAML OperatorSource');
 }
