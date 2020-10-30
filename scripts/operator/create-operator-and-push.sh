d5d2be69fe67925124e15973ddf7e59f8bd5a3e0
Ivan Stanev <istanev93@gmail.com>
2020-10-30 15:36:24 +0000

    chore: creating an Operator image is now a Python script

--- scripts/operator/create-operator-and-push.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-SNYK_MONITOR_IMAGE_TAG="$1"
-SNYK_OPERATOR_IMAGE_NAME_AND_TAG="snyk/kubernetes-operator:${SNYK_MONITOR_IMAGE_TAG}"
-
-./scripts/operator/create-operator.sh "${SNYK_OPERATOR_IMAGE_NAME_AND_TAG}" "${SNYK_MONITOR_IMAGE_TAG}"
-
-docker login --username ${DOCKERHUB_USER} --password ${DOCKERHUB_PASSWORD}
-docker push ${SNYK_OPERATOR_IMAGE_NAME_AND_TAG}
