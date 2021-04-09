402144e59ff84926ce76d48faad70e495b5ad738
ivanstanev <istanev93@gmail.com>
2021-04-09 10:51:48 +0100

    chore: use imagePullSecrets in tests to avoid Docker Hub rate limiting
    This adds a new requirement of having extra environment variables. They are used to create imagePullSecrets to use for our fixtures, thus avoiding getting rate limited by Docker Hub when these images need to be pulled by KinD.
    
    This does not solve the rate limiting from inside snyk-monitor using skopeo, which we've seen only very rarely. If that happens then the workaround is to set the dockercfg.json of snyk-monitor with auth details for Docker Hub.

--- test/setup/platforms/openshift3.ts
+++ test/setup/platforms/openshift3.ts
@@ -1,7 +1,16 @@
 import { platform } from 'os';
-import { download } from '../platforms/kind'; 
+import { download } from '../platforms/kind';
+import { throwIfEnvironmentVariableUnset } from './helpers';
 
 export async function setupTester(): Promise<void> {
   const osDistro = platform();
   await download(osDistro, 'v0.7.0');
+}
+
+export async function validateRequiredEnvironment(): Promise<void> {
+  console.log(
+    'Checking for the required environment variables: DOCKER_HUB_RO_USERNAME, DOCKER_HUB_RO_PASSWORD',
+  );
+  throwIfEnvironmentVariableUnset('DOCKER_HUB_RO_USERNAME');
+  throwIfEnvironmentVariableUnset('DOCKER_HUB_RO_PASSWORD');
 }
