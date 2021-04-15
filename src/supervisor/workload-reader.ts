adc82e6a97bbf86400565b177c42e97ef86b4171
ivanstanev <istanev93@gmail.com>
2021-04-15 16:50:40 +0100

    fix: revert checking for parent workloads being controllers
    This reverts the change in 07f4f34c8fb7a79f8e04e098f91d66015c99a13b.
    DeploymentConfigs on OpenShift create ReplicationControllers which in turn create Pods. However, the ReplicationControllers in this case are not marked as controllers, hence we cannot walk the list of owner references and can never scan DeploymentConfigs.
    
    This change ensures we do not strictly check for controllers to allow us to read DeploymentConfigs.

--- src/supervisor/workload-reader.ts
+++ src/supervisor/workload-reader.ts
@@ -224,10 +224,6 @@ export function getWorkloadReader(workloadType: string): IWorkloadReaderFunc {
 
 export function getSupportedWorkload(ownerRefs: V1OwnerReference[] | undefined): V1OwnerReference | undefined {
   return ownerRefs !== undefined
-    ? ownerRefs.find(
-        (owner) =>
-          SupportedWorkloadTypes.includes(owner.kind) &&
-          owner.controller === true,
-      )
+    ? ownerRefs.find((owner) => SupportedWorkloadTypes.includes(owner.kind))
     : undefined;
 }
