ec4f490c9be64131bdb1e97790992be741ebd28c
ivanstanev <istanev93@gmail.com>
2021-04-15 11:36:48 +0100

    chore: fix Operator tests with OLM on vanilla Kubernetes
    Update OLM to the latest version, which matches the one on OpenShift. This means the existing OperatorSource resource is deprecated, so we can now use CatalogSource instead, similarly to OpenShift.
    
    Also align the CI/CD pipeline to the recent changes in OpenShift tests.

--- test/setup/deployers/operator-olm.ts
+++ test/setup/deployers/operator-olm.ts
@@ -1,5 +1,7 @@
-
 import * as sleep from 'sleep-promise';
+import { parse, stringify } from 'yaml';
+import { readFileSync, writeFileSync } from 'fs';
+
 import { IDeployer, IImageOptions } from './types';
 import * as kubectl from '../../helpers/kubectl';
 
@@ -33,14 +35,37 @@ async function waitToDeployKubernetesOperator(namespace: string): Promise<void> 
 async function deployKubernetesMonitor(
   _imageOptions: IImageOptions,
 ): Promise<void> {
-    await kubectl.applyK8sYaml('./test/fixtures/operator/operator-source-k8s.yaml');
-    await kubectl.applyK8sYaml('./test/fixtures/operator/installation-k8s.yaml');
+  const overriddenOperatorSource = 'snyk-monitor-catalog-source.yaml';
+  createTestOperatorSource(overriddenOperatorSource);
+  await kubectl.applyK8sYaml(overriddenOperatorSource);
+  await kubectl.applyK8sYaml('./test/fixtures/operator/installation-k8s.yaml');
 
-    // Await for the Operator to become available, only then
-    // the Operator can start processing the custom resource.
-    await kubectl.waitForDeployment('snyk-operator', 'marketplace');
-    await kubectl.waitForCRD('snykmonitors.charts.helm.k8s.io');
-    await kubectl.applyK8sYaml('./test/fixtures/operator/custom-resource-k8s.yaml');
-    await waitToDeployKubernetesOperator('marketplace');
-    await kubectl.waitForDeployment('snyk-operator', 'marketplace');
+  // Await for the Operator to become available, only then
+  // the Operator can start processing the custom resource.
+  await kubectl.waitForDeployment('snyk-operator', 'marketplace');
+  await kubectl.waitForCRD('snykmonitors.charts.helm.k8s.io');
+  await kubectl.applyK8sYaml('./test/fixtures/operator/custom-resource-k8s.yaml');
+  await waitToDeployKubernetesOperator('marketplace');
+  await kubectl.waitForDeployment('snyk-operator', 'marketplace');
+}
+
+function createTestOperatorSource(newYamlPath: string): void {
+  console.log('Creating YAML CatalogSource...');
+  const operatorVersion =
+    process.env.OPERATOR_VERSION ?? readFileSync('./.operator_version', 'utf8');
+  const originalCatalogSourceYaml = readFileSync(
+    './test/fixtures/operator/catalog-source-k8s.yaml',
+    'utf8',
+  );
+  const catalogSource: { spec: { image: string } } = parse(
+    originalCatalogSourceYaml,
+  );
+
+  catalogSource.spec.image = catalogSource.spec.image.replace(
+    'TAG_OVERRIDE',
+    operatorVersion,
+  );
+
+  writeFileSync(newYamlPath, stringify(catalogSource));
+  console.log('Created YAML CatalogSource');
 }
