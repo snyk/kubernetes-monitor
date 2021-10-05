0889c8309579b0d3e0346538c83ecdc2e8970461
Ivan Stanev <istanev93@gmail.com>
2021-10-05 11:40:22 +0100

    fix: allow overriding queue size with environment variable
    You can now set the environment variable SNYK_WORKLOADS_TO_SCAN_QUEUE_WORKER_COUNT to override the queue size.

--- test/common/config.spec.ts
+++ test/common/config.spec.ts
@@ -59,4 +59,18 @@ describe('extractNamespaceName()', () => {
       delete process.env.SNYK_CLUSTER_NAME;
     },
   );
+
+  it('loads the expected configuration values', () => {
+    const { config } = require('../../src/common/config');
+    expect(config.AGENT_ID).toEqual(expect.any(String));
+    expect(config.INTEGRATION_ID).toEqual(expect.any(String));
+    expect(config.CLUSTER_NAME).toEqual('Default cluster');
+    expect(config.IMAGE_STORAGE_ROOT).toEqual('/var/tmp');
+    expect(config.EXCLUDED_NAMESPACES).toBeNull();
+    expect(config.HTTPS_PROXY).toBeUndefined();
+    expect(config.HTTP_PROXY).toBeUndefined();
+    expect(config.NO_PROXY).toBeUndefined();
+    expect(config.SKIP_K8S_JOBS).toEqual(false);
+    expect(config.WORKLOADS_TO_SCAN_QUEUE_WORKER_COUNT).toEqual(10);
+  });
 });
