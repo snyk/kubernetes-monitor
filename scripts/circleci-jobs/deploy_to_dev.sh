0c577ea8bbd0871646c3cc8fec6905e7f004e09a
Daniel Kontorovskyi <dan.kontorovskyy@gmail.com>
2022-08-30 16:47:12 +0300

    chore: change deploy to new interanl deploy functionality

---
+++ scripts/circleci-jobs/deploy_to_dev.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+set -e
+
+# Getting latest released tag
+LATEST_TAG_WITH_V=`git describe --abbrev=0 --tags ${CIRCLE_SHA1}`
+LATEST_TAG=${LATEST_TAG_WITH_V:1}-approved
+
+# Send Slack notification
+../slack/notify_deploy.py $LATEST_TAG dev
+
+# Deploy to pre-prod
+git clone https://$GH_TOKEN@github.com/snyk/$KUBERNETES_MONITOR_DEPLOYER_REPO.git
+mkdir -p $KUBERNETES_MONITOR_DEPLOYER_REPO/helm/values/
+cat >$KUBERNETES_MONITOR_DEPLOYER_REPO/helm/values/pre-prod-mt.yaml <<EOF
+clusterName: "Development cluster"
+integrationApi: https://kubernetes-upstream.dev.snyk.io
+log_level: "DEBUG"
+skip_k8s_jobs: true
+
+policyOrgs:
+  - $POLICY_ORG_PRE_PROD
+
+image:
+  tag: $LATEST_TAG
+
+skopeo:
+  compression:
+    level: 1
+
+workers:
+  count: 5
+
+EOF
+cd $KUBERNETES_MONITOR_DEPLOYER_REPO &&
+git commit -am "feat: deploy to dev k-m $LATEST_TAG_WITH_V" &&
+git push origin main
