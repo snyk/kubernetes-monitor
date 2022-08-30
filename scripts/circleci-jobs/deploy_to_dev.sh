ef57b248338762de541e49705a4e3cdff2694bbb
Daniel Kontorovskyi <dan.kontorovskyy@gmail.com>
2022-08-30 19:21:14 +0300

    chore: config git to have an author

--- scripts/circleci-jobs/deploy_to_dev.sh
+++ scripts/circleci-jobs/deploy_to_dev.sh
@@ -3,14 +3,19 @@ set -e
 
 # Getting latest released tag
 LATEST_TAG_WITH_V=`git describe --abbrev=0 --tags ${CIRCLE_SHA1}`
-LATEST_TAG=${LATEST_TAG_WITH_V:1}-approved
+LATEST_TAG=${LATEST_TAG_WITH_V:1}
+LATEST_TAG_APPROVED=${LATEST_TAG}-approved
 
 # Send Slack notification
-./scripts/slack/notify_deploy.py $LATEST_TAG dev
+./scripts/slack/notify_deploy.py $LATEST_TAG_APPROVED dev
 
-# Deploy to pre-prod
+# Config git
+git config --global user.email "k-m@example.com"
+git config --global user.name "K-M Deploy Boy"
+
 git clone https://$GH_TOKEN@github.com/snyk/$KUBERNETES_MONITOR_DEPLOYER_REPO.git
-mkdir -p $KUBERNETES_MONITOR_DEPLOYER_REPO/helm/values/
+
+# Create helm values for different envs
 cat >$KUBERNETES_MONITOR_DEPLOYER_REPO/helm/values/pre-prod-mt.yaml <<EOF
 clusterName: "Development cluster"
 integrationApi: https://kubernetes-upstream.dev.snyk.io
@@ -19,6 +24,29 @@ skip_k8s_jobs: true
 
 policyOrgs:
   - $POLICY_ORG_PRE_PROD
+
+image:
+  tag: $LATEST_TAG_APPROVED
+
+skopeo:
+  compression:
+    level: 1
+
+workers:
+  count: 5
+
+EOF
+
+cat >$KUBERNETES_MONITOR_DEPLOYER_REPO/helm/values/prod-mt.yaml <<EOF
+clusterName: "Production cluster"
+log_level: "DEBUG"
+skip_k8s_jobs: true
+
+limit:
+  memory: "4Gi"
+
+policyOrgs:
+  - $POLICY_ORG_PROD
 
 image:
   tag: $LATEST_TAG
@@ -31,6 +59,7 @@ workers:
   count: 5
 
 EOF
-cd $KUBERNETES_MONITOR_DEPLOYER_REPO &&
-git commit -am "feat: deploy to dev k-m $LATEST_TAG_WITH_V" &&
+
+cd $KUBERNETES_MONITOR_DEPLOYER_REPO
+git commit --allow-empty -am "feat: deploy k-m $LATEST_TAG_WITH_V"
 git push origin main
