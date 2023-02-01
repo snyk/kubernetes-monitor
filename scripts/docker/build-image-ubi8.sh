ce08f0590a13c6eea4c010497e7df084271c9e2e
Sharon Naftaly <sharon.naftaly@snyk.io>
2023-02-01 17:58:52 +0200

    fix: update nodejs install instruction to resolve vuln

--- scripts/docker/build-image-ubi8.sh
+++ scripts/docker/build-image-ubi8.sh
@@ -8,4 +8,9 @@ LOCAL_DISCARDABLE_IMAGE=snyk/kubernetes-monitor:ubi8
 # should happen on merging to `staging`
 NAME_AND_TAG=${1:-$LOCAL_DISCARDABLE_IMAGE}
 
-docker build -t ${NAME_AND_TAG} --file=Dockerfile.ubi8 .
+# This step gets the latest version of node 16 from node.js. It removes the .tar.gz extension and then returns the result.
+# It is used when buidling the ubi image in order to download the latest node 16 version and copy its binary.
+NODE_16_LATEST_VERSION_TAR_GZ_FILE=$(curl --fail --silent https://nodejs.org/dist/latest-v16.x/SHASUMS256.txt | grep linux-x64.tar.gz | awk '{ print $2 }') 
+NODE_16_LATEST_VERSION="${NODE_16_LATEST_VERSION_TAR_GZ_FILE%%.tar.gz}"
+
+docker build --build-arg  NODE_16_LATEST_VERSION="${NODE_16_LATEST_VERSION}" -t ${NAME_AND_TAG} --file=Dockerfile.ubi8 .
