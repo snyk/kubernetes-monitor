7b3462188cf63f8391267b93e8e1c270a810230a
jonny <jonnyowenpowell@gmail.com>
2024-10-23 16:43:34 +0100

    fix: update dependencies to resolve vulns

--- scripts/docker/build-image.sh
+++ scripts/docker/build-image.sh
@@ -11,6 +11,6 @@ LOCAL_DISCARDABLE_IMAGE=snyk/kubernetes-monitor:local
 # should happen on merging to `staging`
 NAME_AND_TAG=${1:-$LOCAL_DISCARDABLE_IMAGE}
 
-docker build -t ${NAME_AND_TAG} .
+docker build --secret id=gh_token,env=GITHUB_PRIVATE_TOKEN -t ${NAME_AND_TAG} .
 
 "$DIR/smoke-test-image-binaries.sh" $NAME_AND_TAG
